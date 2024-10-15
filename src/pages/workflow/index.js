f656cf7aa65b9a267f8063503981f54e7dd66a2a
Mani <manied116@gmail.com>
2024-10-15 20:44:14 +0530

    inital commit

--- src/pages/workflow/index.js
+++ src/pages/workflow/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Col, Row,Switch, Button,Avatar,Badge,Divider} from 'antd';
-import {UserOutlined,CalendarOutlined,LineChartOutlined} from '@ant-design/icons';
+import {UserOutlined,} from '@ant-design/icons';
 
 const Workflow = () => {
     const onChange = (checked) => {
